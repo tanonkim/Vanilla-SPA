21cdc2c74ad8532808931d95e52518e931f08424
Clip <robbins4bos@gmail.com>
2025-05-01 14:55:56 +0900

    docs : js doc 추가

--- src/shared/lib/dom/client.ts
+++ src/shared/lib/dom/client.ts
@@ -1,5 +1,13 @@
 import { VNode } from "../jsx/jsx-runtime/type";
 
+/**
+ * @fileoverview Virtual DOM -> DOM
+ * @description
+ * - 1. null/undefined -> fragment 생성 / string, number -> text 노드 생성
+ * - 2. node.type기반으로 실제 DOM에 element 생성
+ * - 3. Virtual DOM의 props -> 실제 DOM에 반영
+ * - 4. children을 재귀호출로 element에 appendChild
+ */
 const createElement = (node: VNode) => {
   if (node === null || node === undefined) {
     return document.createDocumentFragment();
--- src/shared/lib/jsx/jsx-runtime/index.ts
+++ src/shared/lib/jsx/jsx-runtime/index.ts
@@ -1,5 +1,10 @@
 import { VDOM, VNode } from "./type";
 
+/**
+ * @fileoverview jsx 팩토리 함수 생성
+ * @description
+ * - <div>Hello World</div> 형태의 jsx문법 -> 트랜스파일링 -> h('div',null,['Hello World'])
+ */
 type Component = (props?: Record<string, any>) => VDOM;
 
 export const h = (
